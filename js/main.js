4c06fad370a82a098ae6abacea956372f88176a0
Igor Zalutsky <igor.zalutsky@gmail.com>
2013-02-13 15:14:00 +0300

    Project pagination working

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+(function(){
+
+var currentProject = 0;
+
 $('.jcarousel').each(function(){
 	var carousel = $(this).jcarousel({
 		wrap: 'circular'
 	});
 	var left = carousel.parents('.carousel').find('.jcarousel-prev');
 	var right = carousel.parents('.carousel').find('.jcarousel-next');
-	debugger;
 	left.jcarouselControl({
 		target: '-=1',
 		carousel: carousel
@@ -15,3 +18,15 @@ $('.jcarousel').each(function(){
 	});
 });
 
+$('.submenu a').each(function(index){
+	var projects = $('.project');
+	var current = $(projects[index]);
+	$(this).click(function(e){
+		e.preventDefault();
+		$(projects[currentProject]).addClass('visuallyhidden');
+		$(projects[index]).removeClass('visuallyhidden');
+		currentProject = index;
+	});
+});
+
+}());
