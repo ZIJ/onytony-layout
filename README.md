8ebe666d50796b79e47a534cb45896adcefc11b9
Igor Zalutsky <igor.zalutsky@gmail.com>
2013-02-13 13:53:06 +0300

    jCarousel working

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Homepage layout for one [talented web-designer](http://onytony.com)
 
 Built on [HTML5 Boilerplate](http://html5boilerplate.com)
+
+Slideshow powered by [jCarousel](https://github.com/jsor/jcarousel)
--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+$('.jcarousel').each(function(){
+	var carousel = $(this).jcarousel({
+		wrap: 'circular'
+	});
+	var left = carousel.parents('.carousel').find('.jcarousel-prev');
+	var right = carousel.parents('.carousel').find('.jcarousel-next');
+	debugger;
+	left.jcarouselControl({
+		target: '-=1',
+		carousel: carousel
+	});
+	right.jcarouselControl({
+		target: '+=1',
+		carousel: carousel
+	});
+});
 
